a1ed7c040867977eb469f5f54626e353e2cef628
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-02 19:31:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
+require "pry"
+
 def my_collect(array)
   i = 0
   collection = []
   while i < array.length
     collection.push(yield(array[i]))
     i += 1
+    binding.pry
   end
   collection
 end
